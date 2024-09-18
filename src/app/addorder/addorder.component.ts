17e9a27ffa79a1f438615a2bb7df23f04e237a54
janith <mihiranga.office@gmail.com>
2024-09-18 16:13:46 +0530

    Table creted and when click the data via add the table button the textfild data should be added to the tabele :Done,
    The table Data row Should be Removed if Requierd and remove buttin click

--- src/app/addorder/addorder.component.ts
+++ src/app/addorder/addorder.component.ts
@@ -1,13 +1,50 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addorder',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ReactiveFormsModule,FormsModule],
   templateUrl: './addorder.component.html',
   styleUrl: './addorder.component.scss'
 })
 export class AddorderComponent {
 
-  
+
+  newOrder={
+    // orderId:'',
+    orderName:'',
+    customername:'',
+    customerTele:'',
+    customerAddress:'',
+  }
+
+items:Array<{orderName:string,
+  customername:string,customerTele:string,customerAddress:string}>=[]
+
+addItem(){
+  if(this.newOrder.orderName &&
+    this.newOrder.customername && this.newOrder.customerTele &&
+    this.newOrder.customerAddress 
+  )
+  {
+
+    this.items.push({
+     // orderId:this.newOrder.orderId,
+      orderName:this.newOrder.orderName,
+      customername :this.newOrder.customername,
+      customerTele:this.newOrder.customerTele,
+      customerAddress:this.newOrder.customerAddress
+
+    });
+
+    //for clear after submitting
+    this.newOrder={orderName:'',customername:'',customerTele:'',customerAddress:''}
+   
+  }
+}
+removeItem(){
+  this.items.pop()
+}
 }
