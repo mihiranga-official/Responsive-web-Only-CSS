63f313344b38a17039d1c62f180c2e986f253b79
janith <mihiranga.office@gmail.com>
2024-10-16 15:44:43 +0530

    Firebase Connected but not Working properly

--- src/app/addsupplier/addsupplier.component.ts
+++ src/app/addsupplier/addsupplier.component.ts
@@ -1,41 +1,65 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { collectionData, Firestore } from '@angular/fire/firestore';
 import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { addDoc, collection } from 'firebase/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-addsupplier',
   standalone: true,
   imports: [CommonModule,
-    RouterOutlet, 
+    RouterOutlet,
     FormsModule,
     ReactiveFormsModule],
   templateUrl: './addsupplier.component.html',
   styleUrl: './addsupplier.component.scss'
 })
-export class AddsupplierComponent implements OnInit{
-  
+export class AddsupplierComponent implements OnInit {
+  suppliers$: Observable<any[]>
   addSuppliers!: FormGroup;
 
-  constructor(private fb:FormBuilder){
-
+  constructor(private fb: FormBuilder, private firestore: Firestore) {
+    const suppliersCollection = collection(this.firestore, 'suppliers')
+    this.suppliers$ = collectionData(suppliersCollection, { idField: 'id' })
   }
 
-  ngOnInit():void  {
-   this.addSuppliers =this.fb.group({
-    supplierName:['',Validators.required],
-    supplierCompany:['',[Validators.required,Validators.minLength(10)]],
-    supplierContact:['',Validators.required,Validators.maxLength(10)], 
-    supplierArea:['',Validators.required,Validators.minLength(50)],
-   })
+
+
+
+  ngOnInit(): void {
+    this.addSuppliers = this.fb.group({
+      supplierName: [''],
+      supplierCompany: ['' ],
+      supplierContact: [''],
+      supplierArea: [''],
+    })
   }
-onSubmit(){
-  console.log();
-  console.log();
-  console.log();
-  console.log();
+  onSubmit() {
+    console.log();
+    console.log();
+    console.log();
+    console.log();
+
+  }
   
-}
+  async addSupplier() {
+    if (this.addSuppliers.valid) {
+      
+      const supplierData = this.addSuppliers.value;
+      
+      try {
+        await addDoc(collection(this.firestore, 'suppliers'), supplierData);
+        console.log("HIIIII",supplierData);
+        this.addSuppliers.reset();
+        
+      }
+      catch (error) {
+        console.log('Error adding supplier: ', error);
 
+      }
+    }
+  }
 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,19 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch())]
+  providers: 
+  [provideRouter(routes),
+   provideClientHydration(),
+   provideHttpClient(withFetch()),
+   provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+   provideFirestore(() => getFirestore())
+   
+  ]
 };
