03534e10743213e44352ac1f7450e41d3b378aa0
janith <mihiranga.office@gmail.com>
2024-10-18 16:28:44 +0530

    db Connected

---
+++ src/Env/environments.ts
@@ -0,0 +1,11 @@
+// Firebase configuration object
+export const firebaseConfig = {
+    apiKey: "AIzaSyC75_BPXqJid3oMrL8uw6Zrcj0MoQ62U9A",
+    authDomain: "pos-system-b2816.firebaseapp.com",
+    projectId: "pos-system-b2816",
+    storageBucket: "pos-system-b2816.appspot.com",
+    messagingSenderId: "270909553814",
+    appId: "1:270909553814:web:fbdbaa549c3ae8c53216b7",
+    measurementId: "G-DLP5KYYSH7"
+  };
+  
--- src/app/addsupplier/addsupplier.component.ts
+++ src/app/addsupplier/addsupplier.component.ts
@@ -1,58 +1,59 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { collectionData } from '@angular/fire/firestore';
-import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-
 import { Observable } from 'rxjs';
-import { Firestore, collection, addDoc } from '@angular/fire/firestore';
+import { Database, ref, set, push, getDatabase, onValue } from '@angular/fire/database';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-addsupplier',
   standalone: true,
-  imports: [CommonModule,
-    RouterOutlet,
-    FormsModule,
-    ReactiveFormsModule],
+  imports: [CommonModule, RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './addsupplier.component.html',
-  styleUrl: './addsupplier.component.scss'
+  styleUrls: ['./addsupplier.component.scss']
 })
 export class AddsupplierComponent implements OnInit {
-  suppliers$: Observable<any[]>
+  suppliers$!: Observable<any[]>;
   addSuppliers!: FormGroup;
+  supplierList: any[] = [];
 
-  constructor(private fb: FormBuilder, private firestore: Firestore) {
-    const suppliersCollection = collection(this.firestore, 'suppliers')
-    this.suppliers$ = collectionData(suppliersCollection, { idField: 'id' })
+  constructor(private fb: FormBuilder, private db: Database) {
+    const suppliersRef = ref(this.db, 'suppliers');
+    onValue(suppliersRef, (snapshot) => {
+      const data = snapshot.val();
+      if (data) {
+        this.supplierList = Object.values(data);
+      }
+    });
   }
-
-
-
 
   ngOnInit(): void {
     this.addSuppliers = this.fb.group({
-      supplierName: [''],
-      supplierCompany: ['' ],
-      supplierContact: [''],
-      supplierArea: [''],
-    })
+      supplierName: ['', Validators.required],
+      supplierCompany: ['', Validators.required],
+      supplierContact: ['', Validators.required],
+      supplierArea: ['', Validators.required],
+    });
   }
+
   onSubmit() {
     this.addSupplier();
-
   }
-  
+
   async addSupplier() {
     if (this.addSuppliers.valid) {
       const supplierData = this.addSuppliers.value;
       try {
-        await addDoc(collection(this.firestore, 'suppliers'), supplierData);
-        console.log("Supplier added successfully:", supplierData);
+        const db = getDatabase();
+        const supplierRef = ref(db, 'suppliers');
+        const newSupplierRef = push(supplierRef);
+        await set(newSupplierRef, supplierData);
+        console.log('Supplier added successfully:', supplierData);
         this.addSuppliers.reset();
       } catch (error) {
-        console.log('Error adding supplier:', error);
+        console.error('Error adding supplier:', error);
       }
     }
   }
-
 }
